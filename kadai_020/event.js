ffea4989cf3192dcfbd1144db667ec5c78d32d1b
MST555 <151428708+MST555@users.noreply.github.com>
2024-06-29 15:07:14 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById('btn');
-
+const text = document.getElementById('text');
 btn.addEventListener('click', () => {
-    document.getElementById("text").innerHTML = "ボタンをクリックしました";
+    text.innerHTML = "ボタンをクリックしました";
 });
